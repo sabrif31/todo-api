2d3482315c7657030fc499bf1ba6004b2716d582
Sabri <sabriflorin@gmail.com>
2023-02-26 19:19:15 +0100

    first commit

---
+++ src/article/article.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import {
+  ArticleCreateInput,
+  ArticleCreateOutput,
+} from './dto/article.create.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import {
+  ArticleUpdateInput,
+  ArticleUpdateOutput,
+} from './dto/article.update.dto';
+import { Article } from './models/article.model';
+import { ArticleDeleteOutput } from './dto/article.delete.dto';
+
+@Injectable()
+export class ArticleService {
+  constructor(
+    @InjectRepository(Article)
+    private readonly articleRepository: Repository<Article>,
+  ) {}
+
+  async articleCreate(input: ArticleCreateInput): Promise<ArticleCreateOutput> {
+    const newArticle = this.articleRepository.create(input);
+    const article = await this.articleRepository.save(newArticle);
+    return { article };
+  }
+
+  async articleUpdate(
+    articleId: Article['id'],
+    input: ArticleUpdateInput,
+  ): Promise<ArticleUpdateOutput> {
+    const article = await this.articleRepository.findOneOrFail(articleId);
+    article.title = input.title;
+    article.description = input.description;
+    article.image = input.image;
+    await article.save();
+    return { article };
+  }
+
+  async articleDelete(articleId: Article['id']): Promise<ArticleDeleteOutput> {
+    const article = await this.articleRepository.findOneOrFail(articleId);
+    await article.remove();
+    return { articleId };
+  }
+}
